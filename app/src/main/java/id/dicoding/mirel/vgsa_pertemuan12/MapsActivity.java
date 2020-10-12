0b7c76ce7a95c061e0b71bf6438529bc9c899b73
luizmirrelll <aloysius.mirel96@gmail.com>
2020-10-12 20:57:32 +0700

    error tapi sudah selesai 4 (FIX)

--- app/src/main/java/id/dicoding/mirel/vgsa_pertemuan12/MapsActivity.java
+++ app/src/main/java/id/dicoding/mirel/vgsa_pertemuan12/MapsActivity.java
@@ -96,7 +96,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onResume() {
         super.onResume();
-        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         lokasiManeger.requestLocationUpdates(provider,400,1,this);
